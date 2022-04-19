ca1aed3c3c60578e95a65b725b904db6e2d2da7c
Paulo Baltieri <37188572+baltieri7@users.noreply.github.com>
2022-04-19 20:11:25 -0300

    inserção do bootstrap

--- index.js
+++ index.js
@@ -5,39 +5,12 @@ const app = express()
 app.set("view engine", "ejs")
 app.use(express.static('public'))
 
-app.get('/', (req, res) => [
-    res.send('Olá mundo')
-])
-app.get('/home', (req, res) => {
-    let nome = "Paulo"
-    let sobrenome = "Baltieri"
-    let exibirMsg = false
-    res.render("principal/home", {
-        nome,
-        sobrenome,
-        exibirMsg
-    })
-})
-app.get('/raca/:nome?', (req, res) => {
-    let raca = req.params.nome
-    let porte = "Pequeno"
-    let origem = "Brasil"
-    res.render("principal/raca", {
-        raca,
-        porte,
-        origem
-    })
+app.get('/', (req, res) => {
+    res.render('index')
 })
 
-app.get('/produtos', (req, res) => {
-    let produtos = [
-        { nome: "ração prime", valor: 25.00 },
-        { nome: "Coleira", valor: 12.00 },
-        { nome: "Shampoo", valor: 8.90 }
-    ]
-    res.render("principal/produtos",{
-        produtos
-    })
+app.get('/question', (req, res) => {
+    res.render('question')
 })
 app.listen(8080, () => {
     console.log('App funcionando 🚀 ')
