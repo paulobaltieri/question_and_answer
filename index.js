e7f6c94c9bfb95fbbe7074444977e82fc14ed9e8
Paulo Baltieri <37188572+baltieri7@users.noreply.github.com>
2022-04-25 20:07:17 -0300

    gravando informações no banco

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 //Informa ao Express a view engine usada.
 app.set("view engine", "ejs")
 app.use(express.static('public'))
 
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
     res.render('index')
 })
-
 app.get('/question', (req, res) => {
     res.render('question')
+})
+app.post('/saveQuestion', (req, res) => {
+    let title = req.body.title
+    let descrition = req.body.descrition
+    res.send(`Título: ${title}  Descrição: ${descrition}`)
 })
 app.listen(8080, () => {
     console.log('App funcionando 🚀 ')
